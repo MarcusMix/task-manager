586aefde0f604655318acb9ca52a76ffd1749898
Marcus Sandi <tradedomarcus@gmail.com>
2023-01-12 20:10:27 -0300

    feat: add Task and TaskItem files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Tasks from './components/tasks/Tasks';
+
+function App() {
+
+  return (
+    <>
+     <Tasks/>
+    </>
+  )
+
+}
+
+export default App;
---
+++ src/components/tasks/Tasks.jsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react"
+import axios from 'axios'
+
+// Style
+import './Tasks.scss'
+
+function Tasks () {
+
+    const [tasks, setTasks] = useState([]);
+    
+      const fetchTasks = async () => {
+        try {
+          const {data} = await axios.get("https://fsc-taskmanager-api.up.railway.app/tasks")
+          setTasks(data)
+        } catch (error) {
+          console.log(error)
+        }
+      }
+    
+      useEffect(() => {
+        fetchTasks()
+      }, [])
+    
+
+    return (  
+
+        <div className="tasks-container">
+            <h3>Últimas tarefas</h3>
+
+            <div className="last-tasks">
+                <div className="tasks-list">
+                    {tasks.filter(tarefas => tarefas.isCompleted === false).map(ultimaTarefa => <p>{ultimaTarefa.description}</p>)}
+                </div>
+            </div>
+
+            <h3>Tarefas realizadas</h3>
+
+            <div className="completed-tasks">
+                {tasks.filter(tarefas => tarefas.isCompleted).map(tarefaConcluida => <p>{tarefaConcluida.description}</p>)}
+            </div>
+
+        </div>
+
+    );
+}
+
+export default Tasks;
+
+// {tasks.map(tarefas => <TaskItem propriedade={tarefas} key={tarefas.id}/>)}     
